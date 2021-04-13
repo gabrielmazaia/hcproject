e0cbf9b34eda4d0ade823de374d6d373f65870b9
Gabriel Antonio <gabriel.mazaia@hotmail.com>
2021-04-13 17:44:13 -0300

    Header

---
+++ header.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+	<meta name="facebook-domain-verification" content="8smohrvtgd18nkm6itaerhpom5hjas" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<style>
+		#gallery-1 img{
+			border: 0px !important;
+		}
+		#gallery-1 .gallery-item{
+			position: relative;
+		}
+		.gallery-spacing-custom .elementor-image-gallery .gallery-icon {
+			position: relative;
+			width: 100%;
+			height: 100%;
+		}
+		#gallery-1 .gallery-item:hover .gallery-caption{
+			opacity: 1;
+
+		}
+		#gallery-1 .gallery-caption{
+			    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+						-webkit-transition: all ease-in-out 0.3s 0.3s;
+			-moz-transition: all ease-in-out 0.3s 0.3s; 
+			-o-transition: all ease-in-out 0.3s 0.3s;  
+			transition: all ease-in-out 0.3s 0.3s;
+			opacity: 0;
+			    position: absolute;
+    left: 0;
+    top: 0;
+    background: #000000c4;
+    width: 97%;
+    height: 97%;
+    
+    padding: 10%;
+    color: #FFF;
+    font-family: 'Poppins',sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+			    align-items: center;
+    justify-content: center;
+		}
+		#gallery-1 .gallery-caption h5{
+			font-size: 16px;
+    font-weight: 400;
+		}
+				#gallery-1 .gallery-caption h6{
+			font-size: 14px;
+    font-weight: 400;
+		}
+	</style>
+    <title>Hello, world!</title>
+    <?php wp_head();?>
+    <link rel="stylesheet" href="<?php bloginfo('template_url');?>/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url');?>/js/owl/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url');?>/js/owl/assets/owl.carousel.default.min.css">
+<?php if(!is_checkout()):?>
+	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '1040518519756981');
+	  fbq('track', 'PageView');
+		
+	</script>
+<?php elseif(is_checkout()):?>
+	  	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '1040518519756981');
+	  fbq('track', 'PageView');
+	  fbq('track', 'InitiateCheckout');	
+	</script>
+<?php endif;?>
+	  
+<?php ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-186900641-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-186900641-1');
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none" 
+       src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+	  <!-- End Facebook Pixel Code -->
+
+<script type="209a29497a9fcbcccc8bd554-text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-186900641-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 444553261 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-444553261"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-444553261');
+</script>
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-186900641-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-186900641-1');
+</script>
+  </head>
+  <body>
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
